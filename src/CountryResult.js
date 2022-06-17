daa69f4685bc9ea3598b1ce2fa29fb79b3b577b0
unknown <Daffa>
2022-06-17 13:56:04 +0700

    daffa

--- src/CountryResult.js
+++ src/CountryResult.js
@@ -3,7 +3,6 @@ import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
